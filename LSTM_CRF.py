cf7bf5005e6e09d37fe4015f115b9f0acd9e4955
sjy1243143783 <64339783+sjy1243143783@users.noreply.github.com>
2023-04-14 17:50:13 +0800

    Add files via upload

--- LSTM_CRF.py
+++ LSTM_CRF.py
@@ -2,12 +2,16 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Embedding, Bidirectional, LSTM, Dense, TimeDistributed,Reshape
-from tensorflow_addons.layers import CRF
+from tf2crf import CRF, ModelWithCRFLoss
+# from tf2crf import CRF, ModelWithCRFLoss
+# 
+# from keras_contrib.layers import CRF
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.metrics import f1_score
+from tensorflow.keras.optimizers import Adam
 
-# 定义数据集
+
 class NERDataset():
     def __init__(self, file_path, word_to_ix, tag_to_ix, max_len):
         self.sentences = []
@@ -40,56 +44,57 @@ class NERDataset():
         return len(self.sentences)
 
     def __getitem__(self, idx):
-        return self.sentences[idx], self.tags[idx]
+        X = tf.keras.utils.to_categorical(self.sentences[idx], num_classes=len(word_to_ix))
+        y = tf.keras.utils.to_categorical(self.tags[idx], num_classes=len(tag_to_ix))
+        return X, y
+
 
 
 
 def create_model(max_len, word_vocab_size, tag_vocab_size, embedding_dim, hidden_dim):
-    # 定义输入
     inputs = Input(shape=(max_len,))
-
-    # 定义嵌入层
     embeddings = Embedding(input_dim=word_vocab_size, output_dim=embedding_dim, input_length=max_len)(inputs)
-
-    # 定义双向LSTM层
     lstm = Bidirectional(LSTM(units=hidden_dim, return_sequences=True))(embeddings)
+    dense = TimeDistributed(Dense(tag_vocab_size))(lstm)
 
-    # 为 Dense 层调整形状
-    reshape = Reshape((-1, 2 * hidden_dim))(lstm)
+    crf = CRF(dtype=tf.float32)
+    output = crf(dense)
 
-    # 定义全连接层
-    fc = TimeDistributed(Dense(units=tag_vocab_size))(reshape)
-
-   # 定义CRF层
-    crf = CRF(units=tag_vocab_size)
-    outputs = crf(fc)
-
-    # 定义模型
-    model = Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
+    base_model = Model(inputs, output)
+    model = ModelWithCRFLoss(base_model, sparse_target=True)
+    model.compile(optimizer=Adam(learning_rate=0.001))
     return model
 
 
-# 训练模型
 def train_model(train_dataset, val_dataset, word_vocab_size, tag_vocab_size, embedding_dim, hidden_dim, batch_size, epochs):
     max_len = train_dataset.sentences.shape[1]
     model = create_model(max_len, word_vocab_size, tag_vocab_size, embedding_dim, hidden_dim)
     early_stop = EarlyStopping(monitor='val_loss', patience=3)
+    X_train = train_dataset.sentences
+    y_train = train_dataset.tags
+    X_val = val_dataset.sentences
+    y_val = val_dataset.tags
 
-    model.fit(train_dataset.sentences, train_dataset.tags, validation_data=(val_dataset.sentences, val_dataset.tags), batch_size=batch_size, epochs=epochs, callbacks=[early_stop])
+    model.fit(X_train, y_train, validation_data=(X_val, y_val), batch_size=batch_size, epochs=epochs, callbacks=[early_stop])
 
     return model
 
 # 预测标签序列
 def predict_labels(model, X_test):
-    y_pred = model.predict(X_test)
-    y_pred = np.argmax(y_pred, axis=-1)
+    y_pred = []
+    for x in X_test:
+        x_cat = np.expand_dims(tf.keras.utils.to_categorical(x, num_classes=len(word_to_ix)), axis=0)
+        y = model.predict(x_cat)
+        y_pred.append(np.argmax(y, axis=-1))
+    y_pred = np.squeeze(np.array(y_pred), axis=-1)
     return y_pred
+
+
 
 # 定义标签到索引的映射
 tag_to_ix = {'O': 0, 'B-LOC': 1, 'I-LOC': 2, 'B-PER': 3, 'I-PER': 4, 'B-ORG': 5, 'I-ORG': 6}
-
+num_tags = len(tag_to_ix)
+optimizer = Adam(learning_rate=0.001)
 # 定义单词到索引的映射
 word_to_ix = {}
 with open('msra/train.txt', 'r', encoding='utf-8') as f:
@@ -99,6 +104,7 @@ with open('msra/train.txt', 'r', encoding='utf-8') as f:
             word = line[0]
             if word not in word_to_ix:
                 word_to_ix[word] = len(word_to_ix)
+
 
 # 定义数据集
 train_dataset = NERDataset('msra/train.txt', word_to_ix, tag_to_ix, max_len=200)
@@ -115,4 +121,7 @@ y_true = np.reshape(y_test, (-1,))
 y_pred = np.reshape(y_pred, (-1,))
 f1 = f1_score(y_true, y_pred, average='weighted')
 print('Test F1 score:', f1)
+
+train_dataset=NERDataset('msra/test.txt',word_to_ix,tag_to_ix,max_len=200)
+
 
